27723172a348b687b56d0c827374504e4982ea71
Steve Kimoi <moenisteve@gmail.com>
2022-09-27 07:44:25 +0300

    creating the app component and the starting functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
 import { useState } from 'react'
+import Die from './Die';
+import { nanoid } from 'nanoid';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const hold = () => {
+    console.log(11233)
+  }
+
+  const newNumbers = () => {
+    let numbers = [] 
+
+    for(let i = 0; i <= 9; i++){
+      numbers.push( {
+        value: Math.ceil(Math.random() * 6), 
+        isHeld: false, 
+        id: nanoid()
+      }) 
+    }
+    
+    return numbers; 
+  }
+
+  const [dice, setDice] = useState(newNumbers());
+
+  const dieDivs = dice.map( die => {
+    return (
+      <Die number={die.value} hold={hold} key={die.id}/> 
+    )
+  })
+
+  function resetButton() {
+    setDice(newNumbers())
+  }
+
 
   return (
-    <div className="App">
-      
+    <div className="app">
+        <div className='app-container'>
+
+          <div className='die-container'> 
+             { dieDivs}
+          </div>
+          
+          <div className='reset-button' onClick={resetButton}>
+             Roll Dice
+          </div>
+
+        </div>
     </div>
   )
 }
